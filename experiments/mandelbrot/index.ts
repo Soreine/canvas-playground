61cec78cc3c175146ff6565cf73cb5421d385716
Soreine <hello@soreine.dev>
2020-05-07 00:06:01 +0200

    first mandelbrot

--- experiments/mandelbrot/index.ts
+++ experiments/mandelbrot/index.ts
@@ -1,18 +1,56 @@
+import Complex from 'complex.js';
+import * as R from 'rambda';
+
 const BG_COLOR = 'white';
 let HEIGHT: number;
 let WIDTH: number;
+let CENTER: {
+  x: number;
+  y: number;
+};
 
 // --------------------------------------------------
 // Custom functions
 // --------------------------------------------------
 
+function mandelbrot(c: Complex, z: Complex = new Complex(0, 0)): Complex {
+  const result = z.mul(z).add(c);
+  return result;
+}
+
+function isMandelbrot(x: number, y: number): boolean {
+  const c = new Complex(x, y);
+  let fcn = new Complex(0, 0);
+
+  if (fcn.abs() > 2) return false;
+
+  for (let i = 0; i < 20; i += 1) {
+    fcn = mandelbrot(c, fcn);
+    if (fcn.abs() > 2) return false;
+  }
+
+  return true;
+}
+
 function setup(ctx: CanvasRenderingContext2D) {
-  return undefined;
+  clear(ctx);
+  R.range(0, WIDTH).forEach((px) => {
+    R.range(0, HEIGHT).forEach((py) => {
+      const x = (px - CENTER.x) / (WIDTH / 4);
+      const y = (py - CENTER.y) / (HEIGHT / 4);
+
+      const inSet = isMandelbrot(x, y);
+      if (inSet) {
+        drawPixel(ctx, px, py, '#000000');
+      } else {
+        drawPixel(ctx, px, py, '#ffffff');
+      }
+    });
+  });
 }
 
 function draw(ctx: CanvasRenderingContext2D, time: number, epsilon: number) {
-  clear(ctx);
-  document.body.innerHTML = `time:  ${time}ms ${epsilon}ms`;
+  // clear(ctx);
   return undefined;
 }
 
@@ -25,6 +63,10 @@ function main() {
 
   HEIGHT = canvas.height;
   WIDTH = canvas.width;
+  CENTER = {
+    x: Math.floor(WIDTH / 2),
+    y: Math.floor(HEIGHT / 2),
+  };
 
   setup(ctx);
 
@@ -78,6 +120,16 @@ function clear(ctx: CanvasRenderingContext2D) {
   ctx.fillStyle = BG_COLOR;
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
   ctx.fillStyle = 'rgba(0, 0, 0, 0)';
+}
+
+function drawPixel(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  color = '#000000'
+) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, 1, 1);
 }
 
 export default main;
